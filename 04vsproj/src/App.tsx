b5c995ed7e2660c65d911edecc6a44bc3047613d
crystalchoi <sujeungchoi@gmail.com>
2025-02-16 12:11:40 +0900

    Init

---
+++ 04vsproj/src/App.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return <div className="App"> Test My Name is Test fasdlfasdjflasdf</div>;
+}
+
+export default App;
--- ch04_hook/src/App.tsx
+++ ch04_hook/src/App.tsx
@@ -1,19 +1,22 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
 import './App.css'
+import {Alert} from "./Alert.tsx";
 
 function App() {
   const [count, setCount] = useState(0)
 
-  const unused = 'something';
+  // const unused = 'something';
   return (
     <>
+
+      <Alert heading="Success" closable> Everything is really good </Alert>
       <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
         Learn React and TypeScript
       </a>
       <p className="read-the-docs">
-      Click on the Vite and React logos to learn more
+      Click on the Vite and React logos to learn more.
     </p>
     </>
   )
